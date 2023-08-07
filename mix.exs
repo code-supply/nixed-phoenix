e01463e45fd4063cd16bb2744bb48fa4cb67f853
Andrew Bruce <me@andrewbruce.net>
2023-08-07 19:15:03 +0100

    Remove runtime_tools from extra_applications
    An attempt to avoid wx. Resulting docker image still has wx.

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule MyNewProject.MixProject do
   def application do
     [
       mod: {MyNewProject.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger]
     ]
   end
 
