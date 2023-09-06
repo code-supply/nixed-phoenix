5b910aea93003273fbf27db67d23a141a533c5c7
Andrew Bruce <me@andrewbruce.net>
2023-09-06 17:28:30 +0100

    Fix versions of asset compilers in config.exs

--- config/config.exs
+++ config/config.exs
@@ -31,7 +31,7 @@ config :my_new_project, MyNewProject.Mailer, adapter: Swoosh.Adapters.Local
 
 # Configure esbuild (the version is required)
 config :esbuild,
-  version: "0.18.13",
+  version: "0.19.2",
   default: [
     args:
       ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
@@ -41,7 +41,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.3.2",
+  version: "3.3.3",
   default: [
     args: ~w(
       --config=tailwind.config.js
